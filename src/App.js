c36cd92ca753a8beccc5d612f48e9e7b8f46f4e6
Andrew Barna <abarna@gmail.com>
2016-08-08 14:09:47 -0700

    make table responsive

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
 import {Table, Column, Cell} from 'fixed-data-table';
+import ResponsiveFixedDataTable from 'responsive-fixed-data-table';
+
 
 class CustomCell extends Component{
-	shouldComponentUpdate(){
-    return false;
-  }
   getData(data, index, col){
+    if (col == "Cruise"){
+      console.log(data[index][col]);
+    }
         try{
-          console.log(this);
-          return data[index][col][0].text
+            return data[index][col].map(function(cell){
+              if (cell.href){
+              return (
+                  <li className={cell.status}>
+                    <a href={cell.href}>{cell.text}</a>
+                  </li>
+                  )
+              } else {
+              return (
+                  <li className={cell.status}>
+                    {cell.text}
+                  </li>
+                  )
+              }
+            });
         } catch (e) {
           return "-"
         }
@@ -18,9 +33,11 @@ class CustomCell extends Component{
     const {rowIndex, field, data, ...props} = this.props;
     return (
       <Cell {...props}>
+      <ul>
       {
         this.getData(this.props.data, rowIndex, this.props.col)
       }
+      </ul>
       </Cell>
     );
   }
@@ -31,13 +48,24 @@ class App extends Component {
     super(props);
 
     this.state = {
-		rows:[], columns:[]};
+		  rows:[], columns:[]
+    };
+
+    this.getRowHeight = this.getRowHeight.bind(this);
   }
 
   componentDidMount(){
     this.serverRequest = $.getJSON(this.props.source, function(result){
         this.setState(result);
       }.bind(this));
+  }
+
+  getRowHeight(rowIndex){
+    var row = this.state.rows[rowIndex];
+    var rowItemLengths = Object.keys(row).map(function(key){return row[key].length});
+    const margins = 48;
+    const liHeight = 18;
+    return margins + liHeight * Math.max.apply(null, rowItemLengths);
   }
 
   render() {
@@ -50,19 +78,22 @@ class App extends Component {
             cell={props => (
                 <CustomCell {...props} col={col} data={this.state.rows}/>
                 )}
-            width={200}
+            width={250}
             />
           )
     }.bind(this));
+
+    console.log(this.state.rows);
     return (
-			 <Table
+			 <ResponsiveFixedDataTable
         rowsCount={this.state.rows.length}
         rowHeight={50}
+        rowHeightGetter={this.getRowHeight}
         headerHeight={50}
         width={1000}
         height={500}>
         {cols}
-      </Table>
+      </ResponsiveFixedDataTable>
     );
   }
 }

ab47f1dacfd7e675271148d1641c236363626b67
Andrew Barna <abarna@gmail.com>
2016-11-10 12:44:55 -0800

    use secure ushydro source

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import './index.css';
 import './fixed-data-table.css';
 
 ReactDOM.render(
-  <App source="http://ushydro.ucsd.edu/hydrotable/json"/>,
+  <App source="https://ushydro.ucsd.edu/hydrotable/json"/>,
   document.getElementById('root')
 );

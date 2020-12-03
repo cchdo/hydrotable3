fa596aea94fce85ee7985c66001975aba02e2dd7
Andrew Barna <abarna@gmail.com>
2020-12-03 14:28:23 +0000

    Fix date sorting
    Since the parsing of strings like "2020-Janurary" is implementation and locale dependent, we need to manually do this conversion. The good news is the backend is controlled by us so this should be reliable.

--- src/App.js
+++ src/App.js
@@ -40,6 +40,22 @@ class CustomCell extends Component{
   }
 }
 
+function monthToNum(datestr){
+  datestr = datestr.replace("Janurary", "01")
+  datestr = datestr.replace("February", "02")
+  datestr = datestr.replace("March", "03")
+  datestr = datestr.replace("April", "04")
+  datestr = datestr.replace("May", "05")
+  datestr = datestr.replace("June", "06")
+  datestr = datestr.replace("July", "07")
+  datestr = datestr.replace("August", "08")
+  datestr = datestr.replace("September", "09")
+  datestr = datestr.replace("October", "10")
+  datestr = datestr.replace("November", "11")
+  datestr = datestr.replace("December", "12")
+  return datestr
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -54,23 +70,13 @@ class App extends Component {
   componentDidMount(){
     this.serverRequest = $.getJSON(this.props.source, function(result){
         var rows = result.rows;
-        result.rows = rows.sort(function(a, b){
-          try {
-            var x = a["Dates"][0].text.split("to");
-            var y = b["Dates"][0].text.split("to");
-            x = Date.parse(x[0].trim())
-            y = Date.parse(y[0].trim())
-            var res = x > y ? -1 : x < y ? 1 : 0;
-            return res;
-          } catch (error) {
-            // Fall back to string comparison
-            var x = a["Dates"][0].text;
-            var y = b["Dates"][0].text;
-            var res = x > y ? -1 : x < y ? 1 : 0;
-            return res;
+      result.rows = rows.sort(function (a, b) {
+        var x = monthToNum(a["Dates"][0].text);
+        var y = monthToNum(b["Dates"][0].text);
+        var res = x > y ? -1 : x < y ? 1 : 0;
+        return res;
 
-          }
-        });
+      });
         this.setState(result);
       }.bind(this));
   }
